96847c86e20a8a3ff5031e890c1e0c78e166c579
wizardling1 <igork3210@gmail.com>
2025-05-18 20:54:50 -0400

    added lean.nvim

--- lua/igor/plugins.lua
+++ lua/igor/plugins.lua
@@ -38,4 +38,28 @@ return {
         -- vim.keymap.set('n', '<leader>lk', ':VimtexStop<CR>', { silent = true })
     end
     },
+    {
+	    'Julian/lean.nvim',
+	    event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+	    dependencies = {
+		    'neovim/nvim-lspconfig',
+		    'nvim-lua/plenary.nvim',
+
+		    -- optional dependencies:
+
+		    -- a completion engine
+		    --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
+
+		    -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
+		    -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+		    -- 'andrewradev/switch.vim',        -- for switch support
+		    -- 'tomtom/tcomment_vim',           -- for commenting
+	    },
+
+	    ---@type lean.Config
+	    opts = { -- see below for full configuration options
+		    mappings = true,
+	    }
+    } 
 }
